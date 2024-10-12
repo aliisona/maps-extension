f3b3e023aceb97b1b55cc0d36ea4c931930d1eab
maxence-glt <maxencegilloteaux2023@gmail.com>
2024-10-12 14:32:08 -0400

    algo commit
    Co-authored-by: Chen, Sam Tian Bao <sam_chen@college.harvard.edu>

--- ML/algo.py
+++ ML/algo.py
@@ -1,16 +1,24 @@
 import pandas as pd 
+import csv
 import numpy as np 
 import seaborn as sns
+print("seaborn")
 import matplotlib.pyplot as plt
+print("matplotlib")
 from sklearn.model_selection import train_test_split
+print("sklearn")
 from sklearn.preprocessing import OrdinalEncoder, LabelEncoder, StandardScaler
+print("sklearn2")
 from imblearn.over_sampling import RandomOverSampler
+print("imblearn")
 from sklearn.impute import SimpleImputer
+print("sklearn3")
 import tensorflow as tf
+print("tensorflow")
 
-#Load the Data
+# Load the Data
 
-df = pd.read_csv("C:/Users/chens/AI and Coding/Hackathons/maps-extension/ML/data/crashdata_boston.csv")
+df = pd.read_csv("data/BostonCrashDetails.csv")
 
 cols = ["Crash_Number", "City_Town_Name", "Crash_Date", "Crash_Time", "Crash_Severity", "Maximum_Injury_Severity_Reported", 
         "Number_of_Vehicles", "Total_Nonfatal_Injuries", "Total_Fatal_Injuries", "Manner_of_Collision", "Vehicle_Action_Prior_to_Crash", 
@@ -19,26 +27,50 @@ cols = ["Crash_Number", "City_Town_Name", "Crash_Date", "Crash_Time", "Crash_Sev
         "Distance_From_Nearest_Landmark", "Non_Motorist_Type", "X_Cooordinate", "Y_Cooordinate"]
 
 df.columns = cols 
-print(df.head())
 
 #reorder + take data we need
-cols_keep = ["Road_Surface_Condition", "Ambient_Light", "Weather_Condition", "Maximum_Injury_Severity_Reported" ,"Total_Nonfatal_Injuries", "Total_Fatal_Injuries"]
+cols_keep = ["Road_Surface_Condition", "Weather_Condition", "Maximum_Injury_Severity_Reported" ,"Total_Nonfatal_Injuries", "Total_Fatal_Injuries"]
 df = df[cols_keep]
 
 print(df.head())
 
-#sort our data
-
-#road_surface_condition:
-encoder_road = OrdinalEncoder(categories=[['dry', 'wet', 'ice']])
-encoded_data_road = encoder_road.fit_transform(df[['Road_Surface_Condition']])
-
-#Weather_Condition:
-possible_weather_conditions = ['Sleet, hail (freezing rain or drizzle)', 
-']
 
 
+# #sort our data
+# df_filtered = df[df['weather_condition'] != 'Not Reported']
 
+
+# #road_surface_condition:
+# encoder_road = OrdinalEncoder(categories=[['dry', 'wet', 'ice']])
+# encoded_data_road = encoder_road.fit_transform(df[['Road_Surface_Condition']])
+
+# # Weather_Condition:
+
+
+# unique_entries = sorted(list(set(df['Road_Surface_Condition'])))
+# print(unique_entries)
+
+categories = {
+    'Good_Weather': [],
+    'Okay_Weather': [],
+    'Precipitation': [],
+    'Severe': []
+}
+
+
+
+
+
+# def csvFilter():
+#     with open('data/BostonCrashDetails.csv', newline='') as csvfile:
+#         reader = csv.reader(csvfile, delimiter=' ')
+#         out = []
+
+#         for row in reader:
+#                 print(row[0])
+
+
+# csvFilter()
 
 
 
--- ML/danger_level.py
+++
@@ -1,5 +0,0 @@
-#Imports
-
-
-
-#Call Algo.Py
