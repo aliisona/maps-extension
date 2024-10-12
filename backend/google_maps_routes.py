59c4b1d65cb905a53411c033a626950fb18f6ac8
Marmik Patel <marmikp19@gmail.com>
2024-10-12 14:07:29 -0400

    create rotue for google maps directions api call

--- backend/google_maps_routes.py
+++ backend/google_maps_routes.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import googlemaps
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
+gmaps = googlemaps.Client(key=os.getenv('GOOGLE_MAPS_API_KEY'))
+
 @app.route("/getstepscoordinates")
 def getStepsCoordinates():
-    return "";
+    origin = request.args.get('origin')
+    destination = request.args.get('destination')
+    mode = request.args.get('mode') # "driving" or "walking"
+
+    # origin = "MIT, 77 Massachusetts Ave, Cambridge, MA 02139"
+    # destination = "Harvard Square, Cambridge, MA 02138"
+
+    if not origin or not destination or not mode:
+        return jsonify({"error": "Missing required parameters: origin, destination, or mode"}), 400
+
+    # Get directions
+    directions_result = gmaps.directions(origin, destination, mode=mode)
+
+    # Print the response (for demonstration)
+    print(directions_result)
+
+    return "called";
+
+if __name__ == '__main__':
+    app.run(debug=True)
